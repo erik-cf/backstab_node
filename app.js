69110b434f98c52334edb3d77300ccbdea926dcb
erik-cf <erikcf95@gmail.com>
2020-03-22 20:59:48 +0100

    Testing mongodb connections

--- app.js
+++ app.js
@@ -2,27 +2,40 @@ let express = require('express');
 let app = express();
 var bodyParser = require('body-parser');
 
+const {MongoClient} = require('mongodb');
+
+const uri = "mongodb+srv://admin:P@ssw0rd@m06-cabezueloerik-e7cnn.mongodb.net/test?retryWrites=true&w=majority";
+var databases = "";
+const client = new MongoClient(uri);
+try{
+
+	await client.connect();
+	await listDatabases(client);
+
+}catch(e){
+	console.error(e);
+}finally{
+	await client.close();
+}
+
+
 app.set('view engine', 'ejs');
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 var PORT = process.env.PORT || 8080;
 
-// parse application/json
 app.use(bodyParser.json());
 
-app.get('/ok-login', function (req, res) {
- res.send('¡ERROR! ¡You must arrive here from a POST request from index form!');
-});
-
-app.post('/ok-login', urlencodedParser, function (req, res) {
-	res.render('pages/ok-login', {data: req.body});
-});
-
 app.get('/', (req, res) => {
-  res.send('El servidor funciona perfectament!!');
-  //res.render('pages/index');
+  res.send('El servidor funciona perfectament!! Les database que hi ha son: ' + databases);
 });
 
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => databases + db.name + ", ");//console.log(` - ${db.name}`));
+};
 
 app.listen(PORT, () => console.log('Backstab està corrent al port ' + PORT + '!'));
