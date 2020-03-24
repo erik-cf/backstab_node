4367f47e2d2ae79f71dea7b6eff4197c9774067b
erik-cf <erikcf95@gmail.com>
2020-03-24 22:58:52 +0100

    API works on local

--- app.js
+++ app.js
@@ -8,66 +8,38 @@ app.use(bodyParser.json());
 const uri = process.env.MONGODBURI;
 const dbName = 'Backstab';
 
-const client = new mongoClient(uri);
-
 app.set('view engine', 'ejs');
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-var collections = [];
 var PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Ves a /api/collection!');
 });
-/*
+
 mongoClient.connect(uri, (err, client) => {
-  if(!err){
+  if(!err) {
     const db = client.db(dbName);
     db.listCollections().toArray((err, items) => {
-      items.forEach(item => {
-        collections.push(item.name);
-        /*
-        app.get('/api/' + item.name, urlencodedParser, (req, res) =>{
-          var collection = db.collection(item.name);
-          var docsArray = [];
-          var docs;
-          if(Object.keys(req.query).length === 0){
-            docs = collection.find();
-          }else{ 
-            docs = collection.find(req.query);
+      items.forEach((item) => {
+        var colName = item.name;
+        app.get('/api/' + colName, (req, res) => {
+          if(Object.keys(req.query) === 0){
+            db.collection(colName).find().toArray((err, docs) => {
+              res.send(docs);
+            });
+          }else{
+            db.collection(colName).find(req.query).toArray((err, docs) => {
+              res.send(docs);
+            });
           }
-          docs.forEach(doc => {
-            docsArray.push(doc);
-          }, () => {
-            res.send(docsArray);
-            client.close();
-          });
-        });*/
-     /* });
+          
+        });
+      }, () => {
+        client.close();
+      });
     });
   }
 });
 
-collections.forEach((col, index, cols) => {
-  app.get('/api/' + col, (req, res) => {
-    mongoClient.connect(uri, (err, client) => {
-      let db = client.db('dbName');
-      let collection = db.collection(item.name);
-      let docsArray = [];
-      let docs;
-      if(Object.keys(req.query).length === 0){
-        docs = collection.find();
-      }else{
-        docs = collection.find(req.query);
-      }
-      docs.forEach((doc) => {
-        docsArray.push(doc);
-      }, () => {
-        res.send(docsArray);
-        client.close();
-      });
-    });
-  });
-});
-*/
 app.listen(PORT, () => console.log('Backstab està corrent al port ' + PORT + '!'));
